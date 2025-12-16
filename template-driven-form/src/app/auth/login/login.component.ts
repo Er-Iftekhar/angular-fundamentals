17ebebcb8713d390c6b29ed417cc3dc42770c7a2
Syed Iftekharuddin <syeydy636@gmail.com>
2025-12-16 13:44:04 +0100

    Reset login form for reactive angular forms

--- template-driven-form/src/app/auth/login/login.component.ts
+++ template-driven-form/src/app/auth/login/login.component.ts
@@ -10,40 +10,44 @@ import { debounceTime } from 'rxjs';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
-  private destroyRef = inject(DestroyRef);
 
 
-  constructor(){
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('saved-login-form');
+  
+  // code for template driven forms
+  // private form = viewChild.required<NgForm>('form');
+  // private destroyRef = inject(DestroyRef);
 
-      if(savedForm){
-        const loadedForm = JSON.parse(savedForm);
-        const email = loadedForm.email;
-        setTimeout(() => {
-          this.form().controls['email'].setValue(email);
-        }, 1);
-      }
 
-      const subscription = this.form().valueChanges?.
-        pipe(debounceTime(500))
-        .subscribe({
-          next: (value) => window.localStorage.setItem(
-            'saved-login-form',
-            JSON.stringify({email: value.email})
-          )
-        })
-        this.destroyRef.onDestroy(() => subscription?.unsubscribe());
-    });
+  // constructor(){
+  //   afterNextRender(() => {
+  //     const savedForm = window.localStorage.getItem('saved-login-form');
+
+  //     if(savedForm){
+  //       const loadedForm = JSON.parse(savedForm);
+  //       const email = loadedForm.email;
+  //       setTimeout(() => {
+  //         this.form().controls['email'].setValue(email);
+  //       }, 1);
+  //     }
+
+  //     const subscription = this.form().valueChanges?.
+  //       pipe(debounceTime(500))
+  //       .subscribe({
+  //         next: (value) => window.localStorage.setItem(
+  //           'saved-login-form',
+  //           JSON.stringify({email: value.email})
+  //         )
+  //       })
+  //       this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+  //   });
     
-  }
+  // }
 
-  onSubmit(formData: NgForm){
-    console.log(formData);
-    const email = formData.form.value.email;
-    const password = formData.form.value.password;
-    console.log(email, password);
-    formData.reset();
-  }
+  // onSubmit(formData: NgForm){
+  //   console.log(formData);
+  //   const email = formData.form.value.email;
+  //   const password = formData.form.value.password;
+  //   console.log(email, password);
+  //   formData.reset();
+  // }
 }
