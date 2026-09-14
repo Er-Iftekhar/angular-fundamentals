e96e2a8734065e5710fd6dade6557c28ad711820
Syed Iftekharuddin <syeydy636@gmail.com>
2026-09-14 15:30:13 +0200

    add signal to the child component and make it workable.

--- tables/src/app/components/shared/simple-table/simple-table.component.ts
+++ tables/src/app/components/shared/simple-table/simple-table.component.ts
@@ -1,68 +1,75 @@
 import {
   AfterViewInit,
   Component,
-  EventEmitter,
-  Input,
-  Output,
+  computed,
+  effect,
+  input,
+  output,
   ViewChild,
 } from '@angular/core';
+
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
 import { MatSort, MatSortModule } from '@angular/material/sort';
+
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { Column } from '../../../models/column.model';
+
 import { NgTemplateOutlet } from '@angular/common';
+import { Column } from '../../../models/column.model';
 
 @Component({
   selector: 'app-simple-table',
+  standalone: true,
   imports: [
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule,
     NgTemplateOutlet,
   ],
   templateUrl: './simple-table.component.html',
-  styleUrl: './simple-table.component.scss',
+  styleUrls: ['./simple-table.component.scss'],
 })
 export class SimpleTableComponent implements AfterViewInit {
-  @Output()
-  rowClicked = new EventEmitter<any>();
-
-  @Input()
-  columns: Column[] = [];
-
   @ViewChild(MatSort)
   sort!: MatSort;
 
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
 
-  get displayedColumns(): string[] {
-    return this.columns.map((column) => column.code);
-  }
+  columns = input.required<Column[]>();
 
-  pageSizeOptions: number[] = [5, 10, 25, 50];
+  processes = input<any[]>([]);
+
+  rowClicked = output<any>();
 
   dataSource = new MatTableDataSource<any>();
 
-  @Input()
-  set processes(value: any[]) {
-    this.dataSource.data = value;
+  displayedColumns = computed(() =>
+    this.columns().map((column) => column.code),
+  );
+
+  constructor() {
+    effect(() => {
+      this.dataSource.data = this.processes();
+    });
   }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
+
     this.dataSource.paginator = this.paginator;
   }
 
   applyFilter(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    this.dataSource.filter = input.value.trim().toLowerCase();
+    const inputElement = event.target as HTMLInputElement;
+
+    this.dataSource.filter = inputElement.value.trim().toLowerCase();
 
     this.dataSource.paginator?.firstPage();
   }
